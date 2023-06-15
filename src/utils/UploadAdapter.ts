04bc8bf3bc7a2b3ed5fcdd3dd3e6c010ff2a149e
Kalu Patrick <kalupatrick94@gmail.com>
2023-06-15 23:26:23 +0100

    first commits

--- src/utils/UploadAdapter.ts
+++ src/utils/UploadAdapter.ts
@@ -10,24 +10,21 @@ export function uploadAdapter(loader: FileLoader): UploadAdapter {
 		upload: () => {
 			return new Promise((resolve, reject) => {
 				(async () => {
-					try {
-						const file = await loader.file;
-						const response = await axios.request({
-							method: "POST",
-							url: `${HOST}/upload_files`,
-							data: {
-								files: file,
-							},
-							headers: {
-								"Content-Type": "multipart/form-data",
-							},
-						});
-						resolve({
-							default: `${HOST}/${response.data.filename}`,
-						});
-					} catch (error) {
-						reject(error);
-					}
+					const file = await loader.file;
+					const response = await axios.request({
+						method: "POST",
+						url: `${HOST}/upload_files`,
+						data: {
+							files: file,
+						},
+						headers: {
+							"Content-Type": "multipart/form-data",
+						},
+					});
+					resolve({
+						default: `${HOST}/${response.data.filename}`,
+					});
+
 				})();
 			});
 		},
